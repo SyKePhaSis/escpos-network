0a4c16a0cefd1c7ddaa0019987a4e98492fa0823
SyKePhaSis <leojr.crassaris@hotmail.com>
2023-08-20 19:20:45 +0300

    (feat): Added some basic functionality and moved from arrays to vectors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,107 @@
-use std::net::TcpStream;
-use std::net::ToSocketAddrs;
-use std::io::Write;
-use std::io::Result;
+use std::net::{TcpStream, ToSocketAddrs, Shutdown};
+use std::io::{Write, Result};
 use std::clone::Clone;
+use std::vec::Vec;
+
+const BUFFER_SIZE: usize = 1024;
 
 pub struct NetworkPrinter {
     name: String,
     stream: TcpStream,
     ip: String,
     port: u16,
-    buffer: [u8; 128]
+    buffer: Vec<u8>,
+    settings: u8
 }
 
-pub trait Printer {
-    fn new(name: String, ip: String, port: u16) -> Self;
-    fn feed(&mut self, lines: u8);
-    fn cut(&mut self);
-}
-
-impl Printer for NetworkPrinter {
-    fn new(name: String, ip:String, port: u16) -> NetworkPrinter {
+impl NetworkPrinter {
+    pub fn new(name: String, ip:String, port: u16) -> NetworkPrinter {
         let mut stream = TcpStream::connect((ip.clone(),port)).expect("Couldn't connect to the server!");
 
-        let buffer = [0 as u8; 128];
+        let buffer: Vec<u8> = Vec::new();
+        let settings: u8 = 0x00;
         return NetworkPrinter{
             name,
             stream,
             ip,
             port,
-            buffer
+            buffer,
+            settings
         }
     }
 
-    fn feed(&mut self, lines: u8){
-        let cmd: [u8; 3] = [0x1B, 0x64, lines];
-        self.stream.write(&cmd);
+    fn add_to_buffer(&mut self, buf: Vec<u8>) {
+        self.buffer.extend(buf);
     }
 
-    fn cut(&mut self){
-        let cmd: [u8; 3] = [0x1D, 0x56, 0x01];
-        self.stream.write(&cmd);
+    fn send_buffer(&mut self){
+        self.stream.write(&self.buffer);
+        self.clear_buffer();
+    }
+
+    fn clear_buffer(&mut self){
+        self.buffer.clear();
+    }
+
+    pub fn initialize(&mut self) {
+        let mut cmd: Vec<u8> = vec![0x1B, 0x40];
+        self.add_to_buffer(cmd);
+        self.send_buffer();
+        self.send_settings();
+    }
+
+    fn send_settings(&mut self) {
+        let mut cmd: Vec<u8> = vec![0x1B, 0x21, self.settings];
+        self.add_to_buffer(cmd);
+        self.send_buffer();
+    }
+
+    pub fn underline(&mut self) {
+        self.settings = self.settings ^ 0x80;
+        self.send_settings();
+    }
+
+    pub fn emphasize(&mut self){
+        self.settings = self.settings ^ 0x08;
+        self.send_settings();
+    }
+
+    pub fn double_height(&mut self){
+        self.settings = self.settings ^ 0x10;
+        self.send_settings();
+    }
+
+    pub fn double_width(&mut self){
+        self.settings = self.settings ^ 0x20
+    }
+
+    pub fn character_font(&mut self){
+        self.settings = self.settings ^ 0x01;
+        self.send_settings();
+    }
+
+    pub fn select_codetable(&mut self, value: u8){
+        let mut cmd: Vec<u8> = vec![0x1B, 0x74, value];
+        self.add_to_buffer(cmd);
+        self.send_buffer();
+    }
+
+    pub fn feed(&mut self, lines: u8){
+        let cmd: Vec<u8> = vec![0x1B, 0x64, lines];
+        self.add_to_buffer(cmd);
+        self.send_buffer();
+    }
+
+    pub fn cut(&mut self){
+        let cmd: Vec<u8> = vec![0x1D, 0x56, 0x01];
+        self.add_to_buffer(cmd);
+        self.send_buffer();
+    }
+
+}
+
+impl Drop for NetworkPrinter {
+    fn drop(&mut self) {
+        self.stream.shutdown(Shutdown::Both);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![allow(unused)]
 
 mod lib;
-use lib::NetworkPrinter;
-use lib::Printer;
+use lib::{NetworkPrinter};
 
 fn main(){
-    let mut printer: NetworkPrinter = lib::Printer::new("Printer".to_string(),"192.168.123.100".to_string(),9100);
+    let mut printer: NetworkPrinter = NetworkPrinter::new("Printer".to_string(),"192.168.123.100".to_string(),9100);
     let mut buff = [0 as u8; 128];
     printer.feed(0x10);
     printer.cut();
